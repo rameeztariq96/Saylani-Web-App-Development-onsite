5860c0527f52f39decd9821a922f1cbbc4d2d251
Rameez Tariq <rameezonly1@gmail.com>
2023-08-07 07:04:02 +0500

    Assignment Complete

---
+++ JavaScript-Assignment01/app.js
@@ -0,0 +1,283 @@
+// ************************** JS Assignment_01 chp_01 Q_1**************************
+
+// alert("Asalam-o-alikum");
+
+// ************************** JS Assignment_01 chp_01 Q_2**************************
+
+// alert("Error! Please enter a valid password.");
+
+// ************************** JS Assignment_01 chp_01 Q_3**************************
+
+// alert("Welcome to JS Land... \nHappy codding!");
+
+// ************************** JS Assignment_01 chp_01 Q_4**************************
+
+// alert("Welcome to JS Land...");
+// alert("Happy Coding!");
+
+// ************************** JS Assignment_01 chp_01 Q_5**************************
+
+// var msg ="Hello... I can run JS through my web browser's console";
+// console.log(msg);
+
+// ************************** JS Assignment_01 chp_02 Q_1**************************
+
+// var username="";
+
+// ************************** JS Assignment_01 chp_02 Q_2**************************
+
+// var myName="M.Rameez Tariq";
+// console.log(myName);
+
+// ************************** JS Assignment_01 chp_02 Q_3**************************
+
+// var message="Hello World";
+// alert(message);
+
+// ************************** JS Assignment_01 chp_02 Q_4**************************
+
+// var fulName="Jhone Doe";
+// var age = "15 Years old";
+// var course= "Certified Mobile Application Development";
+
+// alert(fulName);
+// alert(age);
+// alert(course);
+
+// ************************** JS Assignment_01 chp_02 Q_5**************************
+
+// var food="PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(food);
+
+// ************************** JS Assignment_01 chp_02 Q_6**************************
+
+// var email="onlyrameez1@gmail.com";
+// alert("My email address is "+ email);
+
+// ************************** JS Assignment_01 chp_02 Q_7**************************
+
+// var book="A smarter way to learn JavaScript";
+// alert("I am trying to learn from a book "+ book);
+
+// ************************** JS Assignment_01 chp_02 Q_9**************************
+
+// var ajebCChiz="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(ajebCChiz);
+
+// ************************** JS Assignment_01 chp_03 Q_1**************************
+
+// var age= 25;
+// alert ("I am "+ age +" years old.")
+
+// ************************** JS Assignment_01 chp_03 Q_2**************************
+
+// var visits=10;
+// alert("You have visited this site "+visits+" times.");
+
+// ************************** JS Assignment_01 chp_03 Q_3**************************
+
+// var birthYear=1998;
+// alert("My birth year is "+birthYear);
+
+// ************************** JS Assignment_01 chp_03 Q_4**************************
+
+// var visitorName="John Doe";
+// var productTitle="T-shirt(s)";
+// var quantity=15;
+
+// alert(visitorName + " ordered " + quantity +" "+ productTitle + " on XYZ Clothing store.");
+
+// ************************** JS Assignment_01 chp_04 Q_1**************************
+
+// 3 variables can not be declered in a single statement.
+
+// ************************** JS Assignment_01 chp_04 Q_2**************************
+
+// Legal:
+// userResponse
+// userResponseTime 
+// userResponseTimeLimit 
+// respons
+// fulName
+
+// Illegal:
+// var 
+// alert
+// console
+// log
+
+// ************************** JS Assignment_01 chp_05 Q1**************************
+
+//  var num=3;
+// var anotherNum=5;
+// var equal= num + anotherNum;
+// console.log("Sum of "+ num +" and "+ anotherNum+" is "+ equal);
+
+// ************************** JS Assignment_01 chp_05 Q2**************************
+                        // Subtraction
+// var num=3;
+// var anotherNum=5; 
+// var equal= num - anotherNum;
+// console.log("Sum of "+ num +" and "+ anotherNum+" is "+ equal);
+
+                        // Multiplication
+// var num=3;
+// var anotherNum=5;
+// var equal= num * anotherNum;
+// console.log("Sum of "+ num +" and "+ anotherNum+" is "+ equal);
+
+                        //Division
+// var num=3;
+// var anotherNum=5;
+// var equal= num / anotherNum;
+// console.log("Sum of "+ num +" and "+ anotherNum+" is "+ equal);
+
+// ************************** JS Assignment_01 chp_05 Q3**************************
+
+            // //  A
+            // var num;
+            // //  B 
+            // console.log("Value after variable declaration is: ??")
+            // //  C
+            // var num=6;
+            // //  D
+            // console.log("Initial value: " + num);
+            // //  E
+            // num++;
+            // //  F
+            // console.log("Value after increment is: "+ num);
+            // //  G
+            // var num = 7+num;
+            // //  H 
+            // console.log("Value after addition is: "+ num);
+            // //  I
+            // num--;
+            // //  J
+            // console.log("Value after decrement is "+ num);
+            // //  K
+            // var num= num/3;
+            // //  L
+            // console.log("Output: The remainder is: "+ num);
+
+// ************************** JS Assignment_01 chp_05 Q4**************************
+
+// var cost=600;
+// var totalPerson=5;
+// var totalAmount= cost * totalPerson;
+// console.log("Total cost to buy " + totalPerson + " tickets to a movie is "+ totalAmount +" PKR");
+
+// ************************** JS Assignment_01 chp_05 Q5**************************
+// var tabelNum=4;
+// var num=1;
+// var table= tabelNum*num;
+// console.log(tabelNum+"*"+num+"="+table);
+// num++;
+// console.log(tabelNum+"*"+num+"="+table);
+// num++;
+// console.log(tabelNum+"*"+num+"="+table);
+// num++;
+// console.log(tabelNum+"*"+num+"="+table);
+// num++;
+// console.log(tabelNum+"*"+num+"="+table);
+// num++;
+// console.log(tabelNum+"*"+num+"="+table);
+// num++;
+// console.log(tabelNum+"*"+num+"="+table);
+// num++;
+// console.log(tabelNum+"*"+num+"="+table);
+// num++;
+// console.log(tabelNum+"*"+num+"="+table);
+// num++;
+// console.log(tabelNum+"*"+num+"="+table);
+
+// ************************** JS Assignment_01 chp_05 Q6**************************
+//         // A
+// var c=25;
+//         // B
+// var fahrenheitConvert=(c*9/5)+32;
+// console.log(c +"\u00B0 C is "+fahrenheitConvert+"\u00B0F");
+//         // C
+// var f=70;
+//         // D
+// var celsiusConvert= (f-32)*5/9;
+// console.log(f+"\u00B0 F is "+ celsiusConvert+"\u00B0C");
+
+// ************************** JS Assignment_01 chp_05 Q7**************************
+
+// var priceFirst=650;
+// var priceSecond=100;
+// var firstQuantity=3;
+// var secondQuantity=7;
+// var shipping=100;
+
+// var fristTotal=priceFirst*firstQuantity;
+// var secondTotal=priceSecond*secondQuantity;
+
+// var plusShipping=fristTotal+secondTotal+shipping;
+
+// console.log("Price of item 1 is " + priceFirst);
+// console.log("Quantity of item 1 is "+ firstQuantity);
+// console.log("Price of item 2 is "+priceSecond);
+// console.log("Quantity of item 2 is "+secondQuantity);
+// console.log("Shipping charges "+ shipping);
+
+// console.log("Total cost of your order is "+plusShipping);
+
+// ************************** JS Assignment_01 chp_05 Q8**************************
+
+// var totalMarks=1200;
+// var marksObtained=834;
+
+// var percent=marksObtained/totalMarks*100;
+// console.log("Total marks: " +totalMarks);
+// console.log("Marks obtained: "+marksObtained);
+// console.log("Percentage:"+percent+"%");
+
+// ************************** JS Assignment_01 chp_05 Q9**************************
+
+// var dollar=10;
+// var riyal=25;
+
+// var pkr=(dollar*104.80)+(riyal*28);
+// console.log("Total Currency in PKR: "+pkr);
+
+// ************************** JS Assignment_01 chp_05 Q10**************************
+
+// var num=27;
+// var total=num+5*10/2;
+// console.log("Total is: "+ total);
+
+// ************************** JS Assignment_01 chp_05 Q11**************************
+
+// var currentYear=2023;
+// var birthYear=1996;
+
+// var age=currentYear-birthYear;
+// console.log("Current year: "+ currentYear);
+// console.log("Birth year: "+birthYear);
+// console.log("Your Age is: "+age);
+
+// ************************** JS Assignment_01 chp_05 Q12**************************
+
+// var radius=20;
+// var pie=3.142;
+// var circum=2*pie*radius;
+
+// console.log("The Circumference is: " + circum);
+
+// var area=pie*(radius*radius);
+// console.log("The area is: "+area);
+
+// ************************** JS Assignment_01 chp_05 Q13**************************
+// var snack="Lays";
+// var age=15;
+// var maxAge=65;
+// var maxAmount=3;
+
+// var totalSnackes=(maxAge-age)*maxAmount;
+// console.log("Favourite Snack "+snack);
+// console.log("Current age: " +age);
+// console.log("Estimated maximum age: "+maxAge);
+// console.log("Amount of snack per day: "+maxAmount);
+// console.log("You will need "+totalSnackes+" to last you until the ripe old age of "+maxAge);
+
