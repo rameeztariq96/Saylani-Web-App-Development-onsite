2d1e7577217a2b1a8bf3f33c647864f8f8ea990f
Rameez Tariq <rameezonly1@gmail.com>
2023-08-13 23:55:58 +0500

    JavaScript Asssignment Incomplete

---
+++ JavaScript All Assignments/JavaScript-Assignment02/app.js
@@ -0,0 +1,195 @@
+// **********************************Assignment 02 Chapter 06-09 Ques 01**********************************
+
+// var a =10;
+// console.log("Result: \n The value of a is "+a);
+// ++a;
+// console.log("The value of ++a is: "+a);
+// console.log("Now the value of a is: "+a);
+
+// console.log("The value of a++ is: "+a);
+// a++;
+// console.log("Now the value of a is: "+a);
+
+// --a;
+// console.log("The value of --a is: "+a);
+// console.log("The value of a is: "+a);
+
+// console.log("The value of a-- is:"+a);
+// a--;
+// console.log("Now the value of a is:"+a);
+
+// **********************************Assignment 02 Chapter 06-09 Ques 02**********************************
+
+// var a=2 , b=1;
+// var result = --a - --b + ++b + b--;
+
+// //--a; 1
+// //--a - --b; 0
+// //--a - --b + ++b; 1
+// //--a - --b + ++b + b--; 1
+// // 1+0+1+1=3
+
+// console.log("The result is: "+result);
+
+// **********************************Assignment 02 Chapter 06-09 Ques 03**********************************
+
+// var greetings=prompt("Enter Full Name");
+// alert("Hello "+greetings );
+
+// **********************************Assignment 02 Chapter 06-09 Ques 05**********************************
+
+// var num= prompt("Enter a number","5");
+
+// var times=1;
+// var table=num*times;
+
+// console.log(num+" x "+times+" = "+table);
+// ++times;
+// table=num*times;
+
+// console.log(num+" x "+times+" = "+table);
+// ++times;
+// table=num*times;
+
+// console.log(num+" x "+times+" = "+table);
+// ++times;
+// table=num*times;
+
+// console.log(num+" x "+times+" = "+table);
+// ++times;
+// table=num*times;
+
+// console.log(num+" x "+times+" = "+table);
+// ++times;
+// table=num*times;
+
+// console.log(num+" x "+times+" = "+table);
+// ++times;
+// table=num*times;
+
+// console.log(num+" x "+times+" = "+table);
+// ++times;
+// table=num*times;
+
+// console.log(num+" x "+times+" = "+table);
+// ++times;
+// table=num*times;
+
+// console.log(num+" x "+times+" = "+table);
+// ++times;
+// table=num*times;
+
+// console.log(num+" x "+times+" = "+table);
+// ++times;
+// table=num*times;
+
+// **********************************Assignment 02 Chapter 06-09 Ques 06**********************************
+// var subjectOne=prompt("Enter First Subject");
+// var obtainedMarksOne=+prompt("Obatained Marks for First subject");
+// var subjectTwo=prompt("Enter Second Subject");
+// var obtainedMarksTwo=+prompt("Obatained Marks for Second subject");
+// var subjectThree=prompt("Enter Third Subject");
+// var obtainedMarksThree=+prompt("Obatained Marks for Third subject");
+// var totalMarks=100;
+
+// var PercentageOne=(obtainedMarksOne/totalMarks)*100;
+// var PercentageTwo=(obtainedMarksTwo/totalMarks)*100;
+// var PercentageThree=(obtainedMarksThree/totalMarks)*100;
+
+// console.log("Subjects  Total Marks  Obtained Marks  Percentage");
+// console.log(subjectOne+" " +totalMarks +" " + obtainedMarksOne+" " + PercentageOne+"%");
+// console.log(subjectTwo+" " +totalMarks +" " + obtainedMarksTwo+" " + PercentageTwo+"%");
+// console.log(subjectThree+" " +totalMarks +" " + obtainedMarksThree+" " + PercentageThree+"%");
+
+// var totalSubjectMarks = totalMarks*3;
+// var totalObtainedMarks = obtainedMarksOne + obtainedMarksTwo + obtainedMarksThree;
+// var totalpercentage = PercentageOne + PercentageTwo + PercentageThree/100;
+// console.log("        "+totalSubjectMarks +" " + totalObtainedMarks+" " + totalpercentage+"%");
+
+// **********************************Assignment 02 Chapter 09-011 Ques 01**********************************
+
+// var city= prompt("wirte Your city name.");
+// if(city === "karachi"){
+//     console.log("Welcome To The city of lights.")
+// }else{
+//     console.log("Congrats you live in darkness.")
+// }
+
+// **********************************Assignment 02 Chapter 09-011 Ques 02**********************************
+
+// var gender=prompt("Enter Gender");
+// if (gender === "male"){
+//     console.log("Good Morning Sir");
+// }else if(gender === "female"){
+//    console.log("Good Morning Ma'am")
+// }else {
+//     console.log("InCorrect Input")
+// }
+
+// **********************************Assignment 02 Chapter 09-011 Ques 03**********************************
+
+// var signalLight=prompt("Signal Colour");
+// if (signalLight === "red"){
+//     console.log("Must Stop");
+// }else if(signalLight === "yellow"){
+//     console.log("Ready to move")
+// }else if(signalLight === "green"){
+//     console.log("Move on")
+// }else{
+//     console.log("Incorrect signal colour")
+// }
+
+// **********************************Assignment 02 Chapter 09-011 Ques 04**********************************
+
+// var remainingFull= +prompt("Write Remaining Fuel in car");
+// if (remainingFull <= 0.25){
+//     console.log("Please refill the fuel in your car.")
+// }else{
+//     console.log("Remainig Fuel is good. No need to refuel now.")
+// }
+
+// **********************************Assignment 02 Chapter 09-011 Ques 05**********************************
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+// //true
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+// //false
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// //false
+// if (c === 13){
+//     alert("condition 2 is true");
+//     }
+// //true
+// if (++c < 14){
+//     alert("condition 3 is true");
+//     }
+//     //false
+//     if(c === 14){
+//     alert("condition 4 is true");
+//     }
+//     //true
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+// //true
+// if (true){
+//     alert("True");
+//     }
+//     //true
+//     if (false){
+//     alert("False");
+//     }
+//     //false
+
+// **********************************Assignment 02 Chapter 09-011 Ques 06**********************************
