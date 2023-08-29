52a08c65bd2dc59e269adf15cbfda96798597f53
Rameez Tariq <rameezonly1@gmail.com>
2023-08-29 06:32:37 +0500

    Chapter 17-20 Assignment Complete

---
+++ JavaScript-All-Assignments/JavaScript-Assignment04/app.js
@@ -0,0 +1,112 @@
+// **********************************Assignment 04 Chapter 17-20 Ques 01**********************************
+// var arr1=[];
+// var arr2=[];
+
+// **********************************Assignment 04 Chapter 17-20 Ques 02**********************************
+// var arr1=[0,1,2,3]
+// var arr2=[1,0,1,2]
+// var arr3=[2,1,0,1];
+// console.log(arr1,arr2,arr3);
+
+// **********************************Assignment 04 Chapter 17-20 Ques 03**********************************
+
+// for(var i=1; i<=10 ;i++){
+//     console.log(i);   
+// }
+
+// **********************************Assignment 04 Chapter 17-20 Ques 04**********************************
+
+// var userInput=+prompt("Enter a number to print Table");
+// var userLenght=+prompt("Enter the length of table, that you want to print")
+// for(var i=1; i<=userLenght ;i++){
+//     console.log(userInput+" x "+ i + " = "+ userInput*i);
+// }
+
+// **********************************Assignment 04 Chapter 17-20 Ques 05**********************************
+
+// var fruits=["apple","banana","mango","orange","strawberry"]
+// for(var i=0 ; i<fruits.length ; i++){
+//     console.log("Element at index "+ i +" is " +fruits[i]);
+// }
+
+// **********************************Assignment 04 Chapter 17-20 Ques 06**********************************
+
+// console.log("Counting: ");
+// for(var i=1; i<=15;i++){
+//     console.log(i);
+// }
+
+// console.log("Reverse Counting: ")
+// for(var j=10 ; j>=1 ; j-- ){
+//     console.log(j)
+// }
+
+// console.log("Even Number: ")
+// for(var k=0; k<=20;k++){
+//     if( k % 2 ===0 ){
+//         console.log(k);
+//     }
+// }
+
+// console.log("Odd Number: ")
+// for(var l=0; l<=20;l++){
+//     if(l % 2 !== 0){
+//         console.log(l)
+//     }
+// }
+
+// console.log("Series: ")
+// for(var m=1; m<=20; m++){
+//     if(m % 2 ===0){
+//         console.log(m+"K")
+//     }
+// }
+
+// **********************************Assignment 04 Chapter 17-20 Ques 07**********************************
+
+// var items=["cake","apple pie","cookie","chips","patties"];
+// var usersearch = prompt("Welcome to my Bakery. What do you want to order sir/ma'am?");
+// var allItems=false;
+
+// for(var i=0 ; i<items.length ; i++){
+//     if(usersearch === items[i]){
+//         allItems=true;
+//         console.log(usersearch+" is available at index "+ i +" in our bakery");
+//     } 
+// }
+// if(allItems !== true){
+//     console.log("We are sorry. "+usersearch+" is not Available in our bakery");
+// }
+
+// **********************************Assignment 04 Chapter 17-20 Ques 08**********************************
+
+// var A=[24,53,78,91,12];
+// var maxNum= A[0];
+
+// for(var i=0; i < A.length ; i++){
+//     if(A[i] > maxNum){
+//         maxNum= A[i];
+//     }
+// }
+// console.log("Array Items: "+A);
+// console.log("The largest number is: "+maxNum);
+
+// **********************************Assignment 04 Chapter 17-20 Ques 09**********************************
+// var A=[24,53,78,91,12];
+// var min=A[0];
+
+// for(var i=0; i < A.length; i++){
+//     if( min > A[i] ){
+//         min=A[i]
+//     }
+// }
+// console.log("Array Items: "+ A);
+// console.log("The smallest number is: "+ min);
+
+// **********************************Assignment 04 Chapter 17-20 Ques 10**********************************
+
+// for(var i=1; i<=100; i++){
+//     if( i % 5 === 0){
+//         console.log(i);
+//     }
+// }
