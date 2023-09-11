8d250067c6a9415b1505ba84afc2249751f1cc9d
Rameez Tariq <rameezonly1@gmail.com>
2023-09-11 14:50:53 +0500

    JavaScript Assignment 26-30 complete

---
+++ JavaScript-All-Assignments/JavaScript-Assignment05/app.js
@@ -0,0 +1,60 @@
+// **********************************Assignment 05 Chapter 26-30 Ques 01**********************************
+// var userInput= +prompt("Enter a number Positive integer: ");
+// console.log("Number: ",userInput)
+// console.log("Round off value: ",Math.round(userInput));
+// console.log("Floor value: ",Math.floor(userInput));
+// console.log("Ceil value: ",Math.ceil(userInput));
+
+// **********************************Assignment 05 Chapter 26-30 Ques 02**********************************
+// var userInput= +prompt("Enter a number Negative floating integer: ");
+// console.log("Number: ",userInput)
+// console.log("Round off value: ",Math.round(userInput));
+// console.log("Floor value: ",Math.floor(userInput));
+// console.log("Ceil value: ",Math.ceil(userInput));
+
+// **********************************Assignment 05 Chapter 26-30 Ques 03**********************************
+
+// var userInput= +prompt("Enter a value in Negative")
+// console.log("The absolute value of "+userInput+" is "+Math.abs(userInput));
+
+
+// **********************************Assignment 05 Chapter 26-30 Ques 04**********************************
+// var dice= Math.random();
+// var randomDice= Math.ceil(dice*6);
+// console.log("Random dice value: "+randomDice);
+
+// **********************************Assignment 05 Chapter 26-30 Ques 05**********************************
+
+// var heads="Heads";
+// var tails="Tails";
+
+// var randomGeneration= Math.ceil(Math.random()*1);
+// console.log("Random Coin Value: ",randomGeneration);
+
+// if (randomGeneration === 0){
+//     console.log("Random Coin value: ",tails)
+// }else if( randomGeneration === 1){
+//     console.log("Random Coin value: ",heads)
+// }
+
+// **********************************Assignment 05 Chapter 26-30 Ques 06**********************************
+
+// var user = Math.ceil(Math.random()*100);
+// console.log("Random number between 1 and 100:", user);
+
+// **********************************Assignment 05 Chapter 26-30 Ques 07**********************************
+
+// var userInput= parseInt(Math.ceil(prompt("Enter your weight in kilograms")));
+// console.log("The weight of user is "+userInput+" Kilograms");
+
+// **********************************Assignment 05 Chapter 26-30 Ques 08**********************************
+
+// var userInput= parseInt(prompt("Enter a number between 1 to 10"));
+// var secretNum= Math.round(Math.random()*10);
+
+// if (secretNum === userInput){
+//     console.log("Congratulations !!! ", secretNum , " Was correct")
+// }else{
+//     console.log("Try Again :( ")
+//     console.log("Correct Answer was",secretNum);
+// }
