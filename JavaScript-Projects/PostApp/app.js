ebfd9d22cbc1b45b86b6290092f3daf2d7aa9c12
Rameez Tariq <rameezonly1@gmail.com>
2023-10-06 01:14:28 +0500

    Functionality

--- JavaScript-Projects/PostApp/app.js
+++ JavaScript-Projects/PostApp/app.js
@@ -1,5 +1,29 @@
 function submitbtn(){
-    var postinput = document.getElementById("title")
-    console.log(descriptionOne.value)
-    postinput.value="";
+    var posttitle = document.getElementById("title")
+    var postdescription = document.getElementById("descriptionOne")
+
+    if(posttitle.value.trim() && postdescription.value.trim()){
+
+        var posts=document.getElementById("posts")
+        
+    posts.innerHTML +=`<div class="card">
+    <div class="card-header">
+        Post
+        </div>
+    <div class="card-body">
+        <blockquote class="blockquote mb-0">
+            <p>${posttitle.value}</p>
+            <footer class="blockquote-footer">${postdescription.value}</footer>
+        </blockquote>
+    </div>
+    </div>`
+    posttitle.value="";
+    postdescription.value="";
+    }else{
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Please write a Title & description!',
+          })
+    }
 }
